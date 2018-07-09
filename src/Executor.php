ed6d570062a05789451317261674f86a55e99608
Chris Wright <daverandom@php.net>
2018-07-09 08:55:12 +0100

    Extract TransientResource interface

--- src/Executor.php
+++ src/Executor.php
@@ -4,7 +4,7 @@ namespace Amp\Sql;
 
 use Amp\Promise;
 
-interface Executor
+interface Executor extends TransientResource
 {
     /**
      * @param string $sql SQL query to execute.
@@ -39,18 +39,6 @@ interface Executor
      * @throws QueryError If the operation fails due to an error in the query (such as a syntax error).
      */
     public function execute(string $sql, array $params = []): Promise;
-
-    /**
-     * Indicates if the connection to the database is still alive.
-     *
-     * @return bool
-     */
-    public function isAlive(): bool;
-
-    /**
-     * @return int Timestamp of the last time this connection was used.
-     */
-    public function lastUsedAt(): int;
 
     /**
      * Closes the executor. No further queries may be performed.
