dcbe6ff7124781381972dba9bce359451ae7af2c
Aaron Piotrowski <aaron@trowski.com>
2022-04-11 22:27:36 -0500

    TransactionIsolation helper methods

--- src/TransactionIsolation.php
+++ src/TransactionIsolation.php
@@ -8,4 +8,24 @@ enum TransactionIsolation
     case Committed;
     case Repeatable;
     case Serializable;
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::Uncommitted => 'Uncommitted',
+            self::Committed => 'Committed',
+            self::Repeatable => 'Repeatable',
+            self::Serializable => 'Serializable',
+        };
+    }
+
+    public function toSql(): string
+    {
+        return match ($this) {
+            self::Uncommitted => 'READ UNCOMMITTED',
+            self::Committed => 'READ COMMITTED',
+            self::Repeatable => 'REPEATABLE READ',
+            self::Serializable => 'SERIALIZABLE',
+        };
+    }
 }
