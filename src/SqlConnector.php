52a30eededbd396fb314e6f71bb2e54e77835731
Aaron Piotrowski <aaron@trowski.com>
2022-04-11 22:56:21 -0500

    Add cancellation to connect()

--- src/SqlConnector.php
+++ src/SqlConnector.php
@@ -2,11 +2,13 @@
 
 namespace Amp\Sql;
 
+use Amp\Cancellation;
+
 interface SqlConnector
 {
     /**
      * Returns a new database connection based on the given configuration.
      * Implementations may provide further parameters, such as a Cancellation.
      */
-    public function connect(SqlConfig $config): Link;
+    public function connect(SqlConfig $config, ?Cancellation $cancellation = null): Link;
 }
